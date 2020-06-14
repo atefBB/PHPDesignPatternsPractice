c971069d261ae59d98cd863ba25640f8b873ae84
Atef Ben Ali <atef.bettaib@gmail.com>
2020-06-14 13:07:51 +0100

    test: convert `factory method design pattern` test to `pest` test

--- dp/Creational/FactoryMethod/Tests/FactoryMethodTest.php
+++ dp/Creational/FactoryMethod/Tests/FactoryMethodTest.php
@@ -8,19 +8,16 @@ use DesignPatterns\Creational\FactoryMethod\StdoutLogger;
 use DesignPatterns\Creational\FactoryMethod\StdoutLoggerFactory;
 use PHPUnit\Framework\TestCase;
 
-class FactoryMethodTest extends TestCase 
-{
-	public function testCanCreateStdoutLogging()
-	{
-		$loggerFactory = new StdoutLoggerFactory();
-		$logger = $loggerFactory->createLogger();
-		$this->assertInstanceOf(StdoutLogger::class, $logger);
-	}
-	
-	public function testCanCreateFileLogging()
-	{
-		/* $logger = $loggerFactory->createLogger(); */
-		/* $this->assertInstanceOf(FileLogger::class, $logger); */
-		$this->assertTrue(true);
-	}
-}
+uses(TestCase::class);
+
+it('can create a stdout logging', function () {
+	$loggerFactory = new StdoutLoggerFactory();
+	$logger = $loggerFactory->createLogger();
+	$this->assertInstanceOf(StdoutLogger::class, $logger);
+});
+
+it('can create a file logging', function () {
+	$loggerFactory = new FileLoggerFactory(sys_get_temp_dir());	
+	$logger = $loggerFactory->createLogger();
+	$this->assertInstanceOf(FileLogger::class, $logger);
+});
